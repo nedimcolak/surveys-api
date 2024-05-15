5e71f5cd1035e5e391ce34593de4d3dfd0ea64b1
nedimcolak <nedimcolak.dev@gmail.com>
2024-05-15 20:52:44 +0200

    fix: upload issue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
     credentials: true,
   };
   app.enableCors(options);
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
 }
 bootstrap();
