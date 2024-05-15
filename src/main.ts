d0e9bf2039252e497aa5b45ea64f48bc05084749
nedimcolak <nedimcolak.dev@gmail.com>
2024-05-15 14:14:49 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: 'https://surveys-web-two.vercel.app/',
-  //   methods: ['OPTIONS', 'PUT', 'POST', 'PATCH', 'GET'],
-  // });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(3000);
 }
 bootstrap();
